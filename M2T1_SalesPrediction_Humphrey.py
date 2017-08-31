1361aaed730a7dc102952da80d9e1ac6b4e8a52a
AyeeKhalil <31457133+AyeeKhalil@users.noreply.github.com>
2017-08-31 18:02:11 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Humphrey.py
@@ -0,0 +1,16 @@
+#CTI-110
+#M2T1- Sales Prediction
+#Khalil Humphrey
+#8/29/17
+
+#Get the projected total sales.
+Total_sales = float(input('Enter in the estimated total sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = Total_sales * 0.23
+
+#Display the profit
+print('The profit is $', format(profit, ',.2f'))
+
+
+                     
